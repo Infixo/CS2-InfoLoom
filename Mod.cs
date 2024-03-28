4545753e3c6654eaf1d9a1167e2059c87cb2aaee
Infixo <swidwins@gmail.com>
2024-03-28 20:16:41 +0100

    Applied official project template

---
+++ Mod.cs
@@ -0,0 +1,39 @@
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using Game.SceneFlow;
+
+namespace InfoLoom
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(InfoLoom)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private Setting m_Setting;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+            AssetDatabase.global.LoadSettings(nameof(InfoLoom), m_Setting, new Setting(this));
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null)
+            {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+    }
+}
